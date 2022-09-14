a8f33457179b1b7db4ec73eb4500d4e5d62652e5
Daniel Tsvetkov <zvetkovd@gmail.com>
2022-09-14 14:01:12 +0300

    Fix Navigation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
-const handlebars = require('express-handlebars')
+const handlebars = require('express-handlebars');
+const routes = require('./routes')
 const app = express();
+
+
+
+app.use('/static', express.static('public'));
 
 app.engine('hbs', handlebars.engine({
     extname: 'hbs'
@@ -8,10 +13,7 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
-app.use('/static', express.static('public'));
+app.use(routes);
 
-app.get('/', (req, res)=> {
-    res.render('index')
-})
 
 app.listen(5000, ()=> console.log('App is listening on port 5000...'));
