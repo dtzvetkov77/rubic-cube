d19015a13f3c06a8005d9ac2a68ac92b69338298
Daniel Tsvetkov <zvetkovd@gmail.com>
2022-09-15 17:13:37 +0300

    Configure dynamic render of cubes

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,12 @@
-const express = require('express');
+const express = require("express");
 
-
-const homeController = require('./controllers/homeController');
-const cubeController = require('./controllers/cubeController')
+const homeController = require("./controllers/homeController");
+const cubeController = require("./controllers/cubeController");
 
 const router = express.Router();
 
-router.get('/', homeController.index);
-router.get('/about', homeController.about)
-router.use('/cube', cubeController);
+router.get("/", homeController.index);
+router.get("/about", homeController.about);
+router.use("/cube", cubeController);
 
 module.exports = router;
