be3c13016a9e0cc0490c356d8c9eba7e2a531d0b
Daniel Tsvetkov <zvetkovd@gmail.com>
2022-09-15 19:05:54 +0300

    implement search

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,11 @@
-const cubes = require('../db.json');
-const router = require('express').Router();
+const router = require('express').Router(); 
+const cubeService = require('../services/cubeService');
 
 router.get('/', (req, res)=> {
+    let { search, from, to }   = req.query
+;
+    const cubes = cubeService.getAll(search, from, to);
+
     res.render('index', {cubes})
 })
 
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -3,10 +3,19 @@ const path = require('path');
 
 const cubes = require('../db.json');
 
+exports.getAll = (search = '', fromInput, toInput) => {
+    const from = Number(fromInput) || 0;
+    const to = Number(toInput) || 6
+    const result = cubes
+    .filter(x => x.name.toLowerCase().includes(search.toLowerCase()))
+    .filter(x => x.difficultyLevel >= from && x.difficultyLevel <= to);
+    return result;
+}
+
 exports.getOne = (cubeId) => cubes[cubeId]
 
 exports.save = (cube) => {
-cubes.push(cube);
+cubes.push({id: cubes[cubes.length - 1].id + 1, ...cube});
 let textData = JSON.stringify(cubes, '', 4)
   return  fs.writeFile(path.resolve('src', 'db.json'), textData, {encoding: 'utf-8'})
 }
