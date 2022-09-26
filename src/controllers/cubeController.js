59ffdd1aa75cd645056d53426389bb571c26c227
Daniel Tsvetkov <zvetkovd@gmail.com>
2022-09-26 18:08:48 +0300

    Fix details

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -25,14 +25,13 @@ router.post("/create", async (req, res) => {
 });
 
 router.get('/details/:id', async (req, res)=> {
-    const cube = await cubeService.getOne(req.params.id).lean();
-    
+    const cube = await cubeService.getOneDetails(req.params.id).lean();
     res.render('details', {cube})
 })
 
 router.get('/:cubeId/attach-accessory', async (req, res)=> {
   const cube = await cubeService.getOne(req.params.cubeId).lean();
-  const accessories = await accessoryService.getAll().lean(); 
+  const accessories = await accessoryService.getAllWithout(cube.accessories).lean(); 
   res.render('accessory/attach', {cube, accessories})
 })
 
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -3,16 +3,18 @@ const Accessory = require('../models/Accessory')
 
 
 exports.getAll = async (search = '', fromInput, toInput) => {
-  let cubes = await Cube.find().lean();
-    // const from = Number(fromInput) || 0;
-    // const to = Number(toInput) || 6
-    // const result = cubes
-    // .filter(x => x.name.toLowerCase().includes(search.toLowerCase()))
-    // .filter(x => x.difficultyLevel >= from && x.difficultyLevel <= to);
+  const from = Number(fromInput) || 0;
+  const to = Number(toInput) || 6;
+
+  let cubes = await Cube.find({name: {$regex: new RegExp(search, 'i')}})
+  .where('difficultyLevel').lte(to).gte(from)
+  .lean();
     return cubes;
 }
 
-exports.getOne = (cubeId) => Cube.findById(cubeId).populate('accessories');
+exports.getOne = (cubeId) => Cube.findById(cubeId)
+
+exports.getOneDetails = (cubeId) => Cube.findById(cubeId).populate('accessories');
 
 exports.create = (cube) => Cube.create(cube);
 
