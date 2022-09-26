b5b2fc5a817caf200e46b03f8e57f8280ff65272
Daniel Tsvetkov <zvetkovd@gmail.com>
2022-09-26 22:19:42 +0300

    Implement register

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,14 @@ const express = require("express");
 const homeController = require("./controllers/homeController");
 const cubeController = require("./controllers/cubeController");
 const accessoryController = require('./controllers/accessoryController')
+const authController = require('./controllers/authController')
 
 const router = express.Router();
 
 router.use('/', homeController);
 router.use("/cube", cubeController);
 router.use('/accessory', accessoryController);
+router.use('/auth', authController);
+
 
 module.exports = router;
